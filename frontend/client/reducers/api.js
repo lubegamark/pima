c0cd31fcb7bcf123c9dd8360fdaae019707436f4
kraiba <krabz01@gmail.com>
2017-05-27 02:31:51 +0300

    add data reducer

--- frontend/client/reducers/api.js
+++ frontend/client/reducers/api.js
@@ -1,3 +1,30 @@
-/**
- * Created by kraiba on 27/05/2017.
- */
+import {FETCH_DATA, FETCH_DATA_FAILED, FETCH_DATA_FULFILLED} from '../constants';
+
+export default function chartsReducer(state = {
+  data: [],
+  fetching: false,
+  fetched: false,
+  error: null,
+}, action) {
+  switch (action.type) {
+    case FETCH_DATA: {
+      return {...state, fetching: true};
+    }
+    case FETCH_DATA_FAILED: {
+      return {...state, fetching: false, error: action.error};
+    }
+    case FETCH_DATA_FULFILLED: {
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        data: action.payload.data,
+      };
+    }
+    default: {
+      return state;
+    }
+
+  }
+
+}
