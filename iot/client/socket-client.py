89e627cfc4b593aeefbe45dfa95596f05dd10798
Mugisha <buhiirekeneth@gmail.com>
2017-05-27 02:27:32 +0300

    Added a Socket client wrapper class

--- iot/client/socket-client.py
+++ iot/client/socket-client.py
@@ -4,36 +4,36 @@ import time
 import json
 
 
-def on_message(ws, message):
-    print message
+class SocketClient():
 
+    def on_message(self, ws, message):
+        print message
 
-def on_error(ws, error):
-    print error
+    def on_error(self, ws, error):
+        print error
 
+    def on_close(self, ws):
+        print "### closed ###"
 
-def on_close(ws):
-    print "### closed ###"
-
-
-def on_open(ws):
-    def run(*args):
-        for i in range(3):
+    def on_open(self, ws):
+        def run(*args):
+            for i in range(3):
+                time.sleep(1)
+                content = raw_input("Please enter something: ")
+                data = json.dumps({"post": content})
+                print data
+                ws.send(data)
             time.sleep(1)
-            content = raw_input("Please enter something: ")
-            data = json.dumps({"post": content})
-            print data
-            ws.send(data)
-        time.sleep(1)
-        print "thread terminating..."
-    thread.start_new_thread(run, ())
+            print "thread terminating..."
+        thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
+    sock = SocketClient()
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://127.0.0.1:8000/device/dev1/stream/",
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
+                                on_message=sock.on_message,
+                                on_error=sock.on_error,
+                                on_close=sock.on_close)
+    ws.on_open = sock.on_open
     ws.run_forever()
