413519e6bb9b3d74d41fd6c3b4c848f04cf75cea
Lubega Mark <lubegamark@gmail.com>
2017-05-27 19:25:16 +0300

    Test docker-compose with docker compose

--- backend/pima/pima/settings/test.py
+++ backend/pima/pima/settings/test.py
@@ -1,10 +1,23 @@
 from .base import *
 
 DEBUG = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('redis', 6379)],
+        },
+        'ROUTING': 'pima.routing.channel_routing',
     }
 }
