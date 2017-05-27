fd694f018673d5510798a44a5ed3369fc7f8e6ac
kraiba <krabz01@gmail.com>
2017-05-27 09:19:32 +0300

    fix linting

--- frontend/client/components/ListView.js
+++ frontend/client/components/ListView.js
@@ -5,9 +5,9 @@ import {colors} from '../config/styles';
 
 export default class ListViewSample extends React.Component {
   renderItem = (item) => {
-    const color = item.number > 5 ? colors.primary : colors.secondary
+    const color = item.number > 5 ? colors.primary : colors.secondary;
     return <ListItem reading={item} color={color} />;
-  }
+  };
 
   render() {
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
