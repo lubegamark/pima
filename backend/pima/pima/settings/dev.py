6e1c5f845b381789ba21a481ee212b363fbff87c
Lubega Mark <lubegamark@gmail.com>
2017-05-27 13:47:43 +0300

    Create different settings for different environments

---
+++ backend/pima/pima/settings/dev.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+INSTALLED_APPS += ("debug_toolbar",)
---
+++ backend/pima/pima/settings/staging.py
@@ -0,0 +1,23 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['207.154.204.214:8000', '207.154.204.214']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('redis', 6379)],
+        },
+        'ROUTING': 'pima.routing.channel_routing',
+    }
+}
