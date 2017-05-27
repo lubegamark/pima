aa550afc59bb7b66bdd6214153b0d7a91f7d7e21
Lubega Mark <lubegamark@gmail.com>
2017-05-27 20:03:43 +0300

    Specify db for postgres

--- backend/pima/pima/settings/test.py
+++ backend/pima/pima/settings/test.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'test',
         'USER': 'postgres',
         'HOST': 'localhost',
         'PORT': 5432,
