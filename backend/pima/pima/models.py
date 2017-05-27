5cc0d1d44e518ed2b90b8e3ff8924e685d56ae26
Lubega Mark <lubegamark@gmail.com>
2017-05-27 14:51:38 +0300

    Add group name to consumers

--- backend/pima/pima/models.py
+++ backend/pima/pima/models.py
@@ -1,3 +1,6 @@
+import json
+
+from channels import Group
 from django.db import models
 
 
@@ -28,6 +31,13 @@ class Device(BaseModel):
     def location(self):
         return self.latitude, self.longitude
 
+    @property
+    def group_name(self):
+        """
+        Returns the Channels Group name to use for sending notifications.
+        """
+        return "device-%s" % self.id
+
 
 class Reading(BaseModel):
     """
@@ -36,3 +46,19 @@ class Reading(BaseModel):
     """
     value = models.CharField(max_length=255)
     device = models.ForeignKey(Device, related_name='readings')
+
+    def send_notification(self):
+        notification = {
+            "id": self.id,
+            "value": self.value,
+            "created": self._created_at.strftime("%a %d %b %Y %H:%M"),
+        }
+        Group(self.device.group_name).send({
+            # WebSocket text frame, with JSON content
+            "text": json.dumps(notification),
+        })
+
+    def save(self, *args, **kwargs):
+        result = super(Reading, self).save(*args, **kwargs)
+        self.send_notification()
+        return result
