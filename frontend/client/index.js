d6f96aeecfae14233066e8cf13a57afaa5f2fc8e
kraiba <krabz01@gmail.com>
2017-05-27 03:08:27 +0300

    fix linting

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {Main} from './containers';
-import store from './store'
+import store from './store';
 
 export default class Index extends React.Component {
   componentWillMount() {
