3c31f6c64ad8630a16184d313f5ca52d70cc9338
kraiba <krabz01@gmail.com>
2017-05-27 04:24:25 +0300

    add epic middle ware to reducer

--- frontend/client/store/index.js
+++ frontend/client/store/index.js
@@ -1,6 +1,7 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from '../reducers';
+import epicMiddleware from '../epics';
 
-const store = createStore(reducers);
+const store = createStore(reducers,applyMiddleware(epicMiddleware));
 
 export default store;
