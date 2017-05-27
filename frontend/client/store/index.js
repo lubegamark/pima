572f707dfdf2aa92c7f4a2a72d4c005e9ac95ccc
kraiba <krabz01@gmail.com>
2017-05-27 04:24:41 +0300

    fix linting

--- frontend/client/store/index.js
+++ frontend/client/store/index.js
@@ -2,6 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 import reducers from '../reducers';
 import epicMiddleware from '../epics';
 
-const store = createStore(reducers,applyMiddleware(epicMiddleware));
+const store = createStore(reducers, applyMiddleware(epicMiddleware));
 
 export default store;
