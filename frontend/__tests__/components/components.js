bd33dbaa1606a7ca7c11011f6ee71e6020df34a3
kraiba <krabz01@gmail.com>
2017-05-28 09:53:07 +0300

    fix tests

--- frontend/__tests__/components/components.js
+++ frontend/__tests__/components/components.js
@@ -1,37 +1,30 @@
 import 'react-native';
 import React from 'react';
+import { shallow } from 'enzyme';
 import BigCircularUI from '../../client/components/BigCircularUI';
 import SmallCircularUI from '../../client/components/SmallCircularUI';
 import ListItem from '../../client/components/ListItem';
-import ListView from '../../client/components/ListView';
-
-import renderer from 'react-test-renderer';
 
 describe('actions', () => {
 
   it('Small Circle renders correctly', () => {
-    const tree = renderer.create(
+    shallow(
       <SmallCircularUI/>
     );
   });
 
   it('Big Circle renders correctly', () => {
-    const tree = renderer.create(
+    shallow(
       <BigCircularUI/>
     );
   });
 
   it('List Item renders correctly', () => {
-    const tree = renderer.create(
-      <ListItem/>
+    shallow(
+      <ListItem reading={{}} color={'#ffffff'}/>
     );
   });
 
-  it('List View renders correctly', () => {
-    const tree = renderer.create(
-      <ListView/>
-    );
-  })
 
 
 });
