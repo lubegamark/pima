69f9ceb42744e706aa60299a0104b450013a2ff3
kraiba <krabz01@gmail.com>
2017-05-28 06:19:20 +0300

    remove constants to declaration

--- frontend/client/containers/Main.js
+++ frontend/client/containers/Main.js
@@ -43,11 +43,10 @@ class Main extends React.Component {
     }
   }
   render() {
-    const {number, min, max} = this.props;
     let {number, min, max} = this.props;
-    min = min ? min :0
-    max = max ? max :0
-    number = number ? number :0
+    min = min ? min :0;
+    max = max ? max :0;
+    number = number ? number :0;
 
     const mainColor = number > 5 ? colors.secondaryDark : colors.primaryDark;
     return (
